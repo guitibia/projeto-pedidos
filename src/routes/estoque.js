ba861320f3b0983639d6d48d65075c5a186bdeb7
Guilherme <48665996+guitibia@users.noreply.github.com>
2026-06-19 22:42:06 -0300

    feat(estoque): endpoint GET /api/estoque/log retorna log geral de movimentações

--- src/routes/estoque.js
+++ src/routes/estoque.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router  = express.Router();
-const { listEstoque, movimentar, historico } = require('../controllers/estoqueController');
+const { listEstoque, movimentar, historico, logGeral } = require('../controllers/estoqueController');
 router.get('/',                  listEstoque);
+router.get('/log',               logGeral);
 router.post('/:id/movimentacao', movimentar);
 router.get('/:id/historico',     historico);
 
