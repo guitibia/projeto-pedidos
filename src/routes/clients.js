e73a00632faa3ae2042573346f50f4a640e9f7f7
Guilherme <48665996+guitibia@users.noreply.github.com>
2026-07-15 15:05:15 -0300

    feat(pix): desconto PIX por cliente (endpoint + resumo)

--- src/routes/clients.js
+++ src/routes/clients.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createClient, listClients, listClientOrders, deleteClient, clientSummary } = require('../controllers/clientController');
+const { createClient, listClients, listClientOrders, deleteClient, clientSummary, setPixDiscount } = require('../controllers/clientController');
 
 router.post('/', createClient);
 router.get('/', listClients);
 router.get('/:clientId/orders', listClientOrders);
 router.get('/:id/summary', clientSummary);
 router.delete('/:id', deleteClient);
+router.put('/:id/pix-discount', setPixDiscount);
 
 module.exports = router;
